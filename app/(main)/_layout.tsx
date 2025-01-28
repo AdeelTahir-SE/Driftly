961faab833697dc7ac1c75fda222175ec5570daa
Adeel <adeeltahir6a@gmail.com>
2025-01-28 18:13:24 +0500

    --completed main layout tabs--

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,38 +1,50 @@
 import { Tabs } from "expo-router";
-import { Text, View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
+import { styled } from "nativewind";
 
-const TabIcon = ({ focused, color, icon }) => {
+type TabIconProps = {
+  focused: boolean;
+  color: string;
+  icon: any;
+};
+
+const TabIcon = ({ focused, color, icon }: TabIconProps) => {
   return (
-    <View className="flex items-center justify-center border-2 min-h-full min-w-10">
-      <View className="flex items-center justify-center">
-        <Image source={icon} resizeMode="cover" className="w-8 h-8" />
+    <View
+      className={`flex items-center justify-center flex-col rounded-full ${
+        focused ? "bg-orange-500 w-14 h-14 p-8" : "w-12 h-12 p-1"
+      }`}
+    >
+      <Image source={icon} resizeMode="contain" className="w-8 h-8" />
+      <View className="flex justify-center items-center">
+      <Text
+        className={` ${
+          focused ? "text-white font-bold absolute text-xs" : "text-gray-500"
+        }`}
+      >
+        Home
+      </Text>
       </View>
-      <Text style={{ }}>Home</Text>
     </View>
   );
 };
 
-export default function mainLayout() {
+export default function MainLayout() {
   return (
     <Tabs
+      initialRouteName="home"
       screenOptions={{
-        initialRouteName: "home",
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: "orange",
-        tabBarInactiveTintColor: "white",
-
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "gray",
         tabBarStyle: {
           backgroundColor: "#333333",
-          height: 60,
+          height: 70,
           borderRadius: 300,
-          display: "flex",
           flexDirection: "row",
           justifyContent: "space-between",
-          alignItems: "center",
-          padding: 10,
-          overflow: "hidden",
-          paddingBottom: 0,
-          marginHorizontal: 20,
+          alignItems: "flex-start",
+          paddingTop: 10,
+          paddingHorizontal: 20,
         },
       }}
     >
@@ -40,28 +52,29 @@ export default function mainLayout() {
         name="home"
         options={{
           headerShown: false,
-
-          title: "Home",
+          tabBarShowLabel: false,
           tabBarIcon: ({ color, focused }) => (
-            <TabIcon
-              focused={focused}
-              color={color}
-              icon={require("@/assets/images/check.png")}
-            />
+            <TabIcon focused={focused} color={color} icon={require("@/assets/images/home.png")} />
           ),
         }}
       />
       <Tabs.Screen
-        name="map"
+        name="rides"
         options={{
           headerShown: false,
-          title: "Map",
+          tabBarShowLabel: false,
           tabBarIcon: ({ color, focused }) => (
-            <TabIcon
-              focused={focused}
-              color={color}
-              icon={require("@/assets/images/check.png")}
-            />
+            <TabIcon focused={focused} color={color} icon={require("@/assets/images/rides.png")} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon focused={focused} color={color} icon={require("@/assets/images/chat.png")} />
           ),
         }}
       />
@@ -69,13 +82,9 @@ export default function mainLayout() {
         name="profile"
         options={{
           headerShown: false,
-          title: "Profile",
+          tabBarShowLabel: false,
           tabBarIcon: ({ color, focused }) => (
-            <TabIcon
-              focused={focused}
-              color={color}
-              icon={require("@/assets/images/check.png")}
-            />
+            <TabIcon focused={focused} color={color} icon={require("@/assets/images/profile.png")} />
           ),
         }}
       />
